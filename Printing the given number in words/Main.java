2f8a5edcb4a28b25ca3af2c55ff838e95b7dcf1f
sandhyasneha <51491543+sandhyasneha@users.noreply.github.com>
2019-06-13 12:13:10 +0530

    Test code pushed to repository.

---
+++ Printing the given number in words/Main.java
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a;
+  scanf("%d",&a);
+  switch(a)
+  {
+    case 1:
+      printf("one");
+      break;
+    case 2:
+      printf("two");
+      break;
+    case 3:
+      printf("Three");
+      break;
+      case 4:
+      printf("four");
+    case 5:
+      printf("five");
+      break;
+    default:
+      printf("Invalid");
+      return 0;
+}
+}
